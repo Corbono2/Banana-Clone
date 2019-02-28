e64bf726dfb5a06b9d23ee359290daa1ee3998ee
bmbudai <34104915+bmbudai@users.noreply.github.com>
2019-02-27 23:42:46 -0800

    Added a placeholder model to test moving around in, as well as a monkey face that can be moved around.

---
+++ Debugger/Assets/src/Ben/BB_monkeyController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BB_monkeyController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      float forwardSpeed = Input.GetAxis("Vertical");
+      float sideWaysSpeed = Input.GetAxis("Horizontal");
+      CharacterController cc = GetComponent<CharacterController>();
+
+      Vector3 speed = new Vector3(sideWaysSpeed, 0, forwardSpeed);
+      cc.Move(speed);
+    }
+}
