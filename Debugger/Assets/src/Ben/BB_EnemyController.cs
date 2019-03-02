bd81cf6bfbca2562c4d49b77c422e9f34eaaaa42
bmbudai <34104915+bmbudai@users.noreply.github.com>
2019-03-01 23:03:22 -0800

    Added new prefab enemy -- I've made it automatically go to whatever object has the "Player" tag

--- Debugger/Assets/src/Ben/BB_EnemyController.cs
+++ Debugger/Assets/src/Ben/BB_EnemyController.cs
@@ -6,18 +6,27 @@ public class BB_EnemyController : MonoBehaviour
 {
     private UnityEngine.AI.NavMeshAgent myAgent;
     Transform target;
+    private Animator myAnimator;
 
     // Start is called before the first frame update
     void Start() {
       myAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
       target = GameObject.FindGameObjectWithTag("Player").transform;
-      // Transform armiture = GameObject.FindGameObjectWithTag("armiture").transform;
-      // armiture.GetComponent<Animation>().Play("Spider_Armiture|walk_ani_vor");
+      myAnimator = GetComponent<Animator>();
+      myAnimator.Play("qiun@move_forward");
     }
 
     // Update is called once per frame
     void Update()
     {
       myAgent.SetDestination(target.position);
+      // Check if we've reached the destination
+      if (!myAgent.pathPending) {
+          if (myAgent.remainingDistance <= myAgent.stoppingDistance) {
+              if (!myAgent.hasPath || myAgent.velocity.sqrMagnitude == 0f) {
+                myAnimator.Play("quin@attack_right");
+              }
+          }
+      }
     }
 }
