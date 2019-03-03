0f3266f7f731e89365f1c0f1f190e975e4e83c8c
Luis Lopez <luis13lopez@live.com>
2019-03-02 20:33:42 -0800

    Character can walk forward

---
+++ Debugger/Assets/Scripts/Luis/PlayerCharacterScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacterScript : MonoBehaviour
+{
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       float horizontalVal =  Input.GetAxis("Horizontal");
+     if(horizontalVal == 0){
+animator.SetBool("move",false);
+     }else{
+         if(horizontalVal>0 ){
+             animator.SetBool("move",true);
+         }
+     }
+    }
+}
