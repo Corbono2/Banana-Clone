86d5e6f3f71bda1583fb8412bff888c110784db5
Luis Lopez <luis13lopez@live.com>
2019-03-05 03:55:59 -0800

    Revert "Can shoot"
    This reverts commit fc9d1e8c14fd4fd7e98b8ba1b5408784659d3563.

--- Debugger/Assets/Scripts/Josh/TileTaken.cs
+++ Debugger/Assets/Scripts/Josh/TileTaken.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TileTaken : MonoBehaviour
+public class Tile_Taken : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
--- Debugger/Assets/Scripts/Luis/PlayerCharacterScript.cs
+++ Debugger/Assets/Scripts/Luis/PlayerCharacterScript.cs
@@ -5,9 +5,6 @@ using UnityEngine;
 public class PlayerCharacterScript : MonoBehaviour
 {
     private Animator animator;
-    private float timePassed = 0f;
-    private float keyDelay = 1f;
-    public ProjectileScript myProjectile;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,15 +14,7 @@ public class PlayerCharacterScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       float horizontalVal =  Input.GetAxis("Vertical");
-       bool shoot = Input.GetKey("space");
-       timePassed += Time.deltaTime;
-       if(shoot && timePassed >= keyDelay){
-           timePassed = 0f;
-            myProjectile = Instantiate(myProjectile, animator.bodyPosition, Quaternion.identity);
-             myProjectile.GetComponent<Rigidbody>().AddForce(transform.forward * 150);
-           //Instantiate(myProjectile);
-       }
+       float horizontalVal =  Input.GetAxis("Horizontal");
      if(horizontalVal == 0){
 animator.SetBool("move",false);
      }else{
