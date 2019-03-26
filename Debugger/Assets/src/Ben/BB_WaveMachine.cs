f10c4eebfc79a23ec4eee0efeddb6bcfb9194c16
bmbudai <34104915+bmbudai@users.noreply.github.com>
2019-03-25 17:29:39 -0700

    Wave machine can spawn enemies that move towards the player using the navmesh

---
+++ Debugger/Assets/src/Ben/BB_WaveMachine.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BB_WaveMachine : MonoBehaviour
+{
+    public GameObject enemy;
+    public Transform[] spawnPoints;
+    public float spawnTime = 3f;
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      BeginSpawning();
+    }
+
+    void BeginSpawning() {
+      InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Spawn() {
+      if(target == null) {
+        return;
+      }
+
+      int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+      Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+
+    }
+}
