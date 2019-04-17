c5a88f234c77df49e7012f5445ca02ae1c0c1560
joshkhtan <joshkhtan@gmail.com>
2019-04-17 12:18:40 -0700

    moved my files

---
+++ Debugger/Assets/src/Josh/EnemyMove.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    public float MovementSpeed;   // Holds the value of movement speed
+    public bool isBlocked;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Make the enemy move left over time, towards the end goal
+        transform.Translate(Vector3.back * MovementSpeed * Time.deltaTime);
+    }
+}
