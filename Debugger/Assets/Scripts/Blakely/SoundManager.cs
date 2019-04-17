3869704c935c86efc7a8fb80b4f9b2a7d196e9f4
Blake <bn.frechette@gmail.com>
2019-04-16 23:26:14 -0700

    Recommit sound prefabs

---
+++ Debugger/Assets/Scripts/Blakely/SoundManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance = null;
+    public Sound[] sounds;
+
+    
+    void Awake()
+    {
+        if (instance == null)
+		{
+			instance = this;
+		}
+		else if (instance != this)
+		{
+			Destroy(gameObject);
+		}
+
+        DontDestroyOnLoad(gameObject);
+
+
+        foreach(Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+        }
+
+    }
+
+    public void Play(string soundID)
+    {
+        Sound s = Array.Find(sounds, item => item.soundID == soundID);
+        s.source.Play();   
+    }
+
+}
