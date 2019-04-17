784ad5a36f5f8188f10810c6fc6a9b331d5df1a5
Benjamin Budai <benjamin+gitlab@budai.com>
2019-04-17 00:23:30 +0000

    Revert "Merge branch 'master' of gitlab.com:bbudai/banana"
    This reverts commit 499d1c08d4cce8d489946a21ac24cccaca92e892

--- Debugger/Assets/src/Ben/BB_EnemyController.cs
+++ Debugger/Assets/src/Ben/BB_EnemyController.cs
@@ -9,7 +9,7 @@ public class BB_EnemyController : MonoBehaviour
     private Animator myAnimator;
 
     // Start is called before the first frame update
-    void OnEnable() {
+    void Start() {
       myAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
       target = GameObject.FindGameObjectWithTag("Player").transform;
       myAnimator = GetComponent<Animator>();
@@ -19,11 +19,6 @@ public class BB_EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      if(target == null) {
-        myAnimator.Play("quin@idle");
-        return;
-      }
-
       myAgent.SetDestination(target.position);
       // Check if we've reached the destination
       if (!myAgent.pathPending) {
@@ -31,14 +26,7 @@ public class BB_EnemyController : MonoBehaviour
               if (!myAgent.hasPath || myAgent.velocity.sqrMagnitude == 0f) {
                 myAnimator.Play("quin@attack_right");
               }
-              else {
-                myAnimator.Play("qiun@move_forward");
-              }
           }
       }
     }
-
-    // void OnAnimatorMove() {
-    //   myAgent.speed = (myAnimator.deltaPosition / Time.deltaTime).magnitude;
-    // }
 }
