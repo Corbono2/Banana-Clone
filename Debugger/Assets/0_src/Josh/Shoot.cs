0d9b07dcc2c7b911e4a8fd9217789bcbfd05d9f4
hallmanben <hallmanben@gmail.com>
2019-04-25 18:19:44 -0700

    Added Easter Eggs
    Pressing the 'H' key now restores full health and pressing the 'E' key switches to a special bullet.

--- Debugger/Assets/0_src/Josh/Shoot.cs
+++ Debugger/Assets/0_src/Josh/Shoot.cs
@@ -6,16 +6,22 @@ using System.Collections.Generic;
 //Attach to object being fired from
 public class Shoot : MonoBehaviour
 {
+    bool specialBulletActive;
+
     float bulletSpeed = 500;    //Force of bullet fired
     public GameObject bullet1;   //Allows selection of gameobject in inspector
     public GameObject bullet2;   //Allows selection of gameobject in inspector
+    public GameObject specialBullet; // Adds the special bullet easter egg - Benjamin (Dream Team)
 
     public int pooledAmount;    //Allows change of pooled value in inspector
     List<GameObject> bulletList;    //List to hold bullets
     List<GameObject> bulletList2;    //List to hold bullets
+    List<GameObject> specialBulletList;    //List to hold bullets
 
     void Start()
     {
+        specialBulletActive = false;
+
         //Instantiate bullets on start
         bulletList = new List<GameObject>();
         for (int i = 0; i < pooledAmount; i++)
@@ -34,14 +40,24 @@ public class Shoot : MonoBehaviour
             objBullet.SetActive(false);
             bulletList2.Add(objBullet);
         }
+
+        //Instantiate bullets on start
+        specialBulletList = new List<GameObject>();
+        for (int i = 0; i < pooledAmount; i++)
+        {
+            GameObject objBullet = (GameObject)Instantiate(specialBullet);
+            objBullet.SetActive(false);
+            specialBulletList.Add(objBullet);
+        }
     }
 
     //If bullet currently inactive in hierarchy, fire it
     void Fire()
     {
-        for (int i = 0; i < bulletList.Count; i++)
-        {
-            
+        if (!specialBulletActive) {
+            for (int i = 0; i < bulletList.Count; i++)
+            {
+
                 if (!bulletList[i].activeInHierarchy)
                 {
                     bulletList[i].transform.position = transform.position;
@@ -51,7 +67,25 @@ public class Shoot : MonoBehaviour
                     tempRigidBodyBullet.AddForce(tempRigidBodyBullet.transform.forward * bulletSpeed);
                     break;
                 }
-            
+
+            }
+        }
+        else
+        {
+            for (int i = 0; i < specialBulletList.Count; i++)
+            {
+
+                if (!specialBulletList[i].activeInHierarchy)
+                {
+                    specialBulletList[i].transform.position = transform.position;
+                    specialBulletList[i].transform.rotation = transform.rotation;
+                    specialBulletList[i].SetActive(true);
+                    Rigidbody tempRigidBodyBullet = specialBulletList[i].GetComponent<Rigidbody>();// *****************If Destroyed this is where the error is maybe?
+                    tempRigidBodyBullet.AddForce(tempRigidBodyBullet.transform.forward * bulletSpeed);
+                    break;
+                }
+
+            }
         }
 
     }
@@ -62,6 +96,11 @@ public class Shoot : MonoBehaviour
         if (Input.GetButtonDown("Fire1"))
         {
             Fire();
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            specialBulletActive = true;
         }
     }
 }
